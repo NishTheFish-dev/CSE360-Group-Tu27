377a014ea986588c8e5d27c9956d0040544add5e
games <games@HPLP>
2024-10-03 14:51:56 -0700

    edited the gui slightly

--- GroupTu27/src/application/Main.java
+++ GroupTu27/src/application/Main.java
@@ -11,9 +11,10 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			Scene scene = new Scene(root,1000,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("CSE360 Helpdesk (Group Tu27)");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
