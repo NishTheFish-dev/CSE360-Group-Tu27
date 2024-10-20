2162b3ace2e5a09567cedd0bfb04995c734a3eee
games <games@HPLP>
2024-10-20 13:16:27 -0700

    updated the version number
    Merge branch 'main' of
    https://github.com/NishTheFish-dev/CSE360-Group-Tu27 into main

--- GroupTu27/src/application/Main.java
+++ GroupTu27/src/application/Main.java
@@ -16,7 +16,7 @@ import javafx.stage.Stage;
  * and choose roles. Admin role allows multiple privileges, mainly invitation of other users.
  * All other roles are currently unable to do anything besides logout.
  * 
- * @version 0.7.4
+ * @version 1.2.0
  **/
 
 public class Main extends Application {
