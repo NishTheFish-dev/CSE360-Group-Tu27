5447fdf062daef1f36098a96ba8dd1487b6c419f
Luke <69825619+LukePerezz@users.noreply.github.com>
2024-10-27 19:08:45 -0700

    Added DB stuff to HelpArticleService

--- GroupTu27/src/services/HelpArticleService.java
+++ GroupTu27/src/services/HelpArticleService.java
@@ -3,13 +3,60 @@ package services;
 import models.HelpArticle;
 
 import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.*;
 import java.util.stream.Collectors;
+
 
 public class HelpArticleService {
     private static HelpArticleService instance;
     private List<HelpArticle> articles; // Store articles
+    static final String JDBC_DRIVER = "org.h2.Driver";   
+	static final String DB_URL = "jdbc:h2:~/360projectDB";  
 
+	//  Database credentials 
+	static final String USER = "sa"; 
+	static final String PASS = ""; 
+
+	private Connection connection = null;
+	private Statement statement = null; 
+	//	PreparedStatement pstmt
+	
+	
+	
+	public void DatabaseHelper() throws Exception {
+		
+	}
+	
+	private void createTables() throws SQLException {
+		
+		String postTable = "CREATE TABLE IF NOT EXISTS helpArticles ("
+				+ "id BIGINT AUTO_INCREMENT PRIMARY KEY, "
+				+ "header VARCHAR(255) UNIQUE, "
+				+ "title VARCHAR(255), "
+				+ "shortDesc VARCHAR(255), "
+				+ "searchMaterial VARCHAR(255), "
+				+ "body VARCHAR(255), "
+				+ "referenceLinks VARCHAR(255), "
+				+ "otherInfo VARCHAR(255))";
+		statement.execute(postTable);
+	}
+	
+	public void connectToDatabase() throws SQLException {
+		try {
+			Class.forName(JDBC_DRIVER); // Load the JDBC driver
+			System.out.println("Connecting to database...");
+			connection = DriverManager.getConnection(DB_URL, USER, PASS);
+			statement = connection.createStatement(); 
+			createTables();  // Create the necessary tables if they don't exist
+		} catch (ClassNotFoundException e) {
+			System.err.println("JDBC Driver not found: " + e.getMessage());
+		}
+	}
+	
     private HelpArticleService() {
         this.articles = new ArrayList<>();
     }
